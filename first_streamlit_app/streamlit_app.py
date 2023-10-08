bc40304ae2ef7ec2c22d8a0aacde65728bb52e82
smalatpure <49256802+smalatpure@users.noreply.github.com>
2023-10-08 15:07:52 -0700

    Update streamlit_app.py
    Fixed code bug in read_csv.

--- first_streamlit_app/streamlit_app.py
+++ first_streamlit_app/streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+import pandas pd
 
 streamlit.title('Our Happy Healthy Diner')
 
@@ -11,7 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
 
