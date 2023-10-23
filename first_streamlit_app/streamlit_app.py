47d28670a35cc4535c5641767c4931455f711852
smalatpure <49256802+smalatpure@users.noreply.github.com>
2023-10-23 13:42:45 -0700

    Update streamlit_app.py
    Added exception block

--- first_streamlit_app/streamlit_app.py
+++ first_streamlit_app/streamlit_app.py
@@ -26,15 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API response
 streamlit.header('FruityVice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  # streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # streamlit.text(fruityvice_response.json())
 
-# Normalize and display json response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())                       
-streamlit.dataframe(fruityvice_normalized)
-
+    # Normalize and display json response
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())                       
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
